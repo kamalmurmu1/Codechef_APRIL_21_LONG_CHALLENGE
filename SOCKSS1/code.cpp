b54a21d96b2ec2dbdfcc26eb56b9e88748486bbf
DontknoW-wipe <kamalzero167@gmail.com>
2021-04-06 11:18:21 +0530

    commited codechef 1st question april 21

---
+++ SOCKSS1/code.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int solve()
+{
+    return 0;
+}
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==b||a==c||b==c)
+    {
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
